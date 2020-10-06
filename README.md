0f261d30317199a23bcdc976d0ca1a68e37638d3
Pramod Anantharam <pramod.atre@gmail.com>
2020-10-06 19:10:03 -0400

    initial checkout of files for crazyflie port of projects from udacity

---
+++ README.md
@@ -0,0 +1,24 @@
+# Setup
+
+## Software
+Clone the repository
+
+```shell
+git clone https://github.com/pramodatre/FCND-projects-crazyflie-port.git
+```
+
+Create a virtual environment and activate
+
+This project is tested to work with python 3.6.8 and I recommend using `pyenv` to manage your python versions if you are using MacOS.
+```shell
+python3 -m venv fcnd
+source fcnd/bin/activate
+```
+
+Install all dependencies
+```python
+pip install -r requirements.txt
+```
+
+## Hardware
+It's assumed that you have followed [instructions on setting up Crazyflie2.1](https://www.bitcraze.io/documentation/tutorials/getting-started-with-crazyflie-2-x/). Ensure you have plugged-in Crazyflie2.1 PA radio and the Crazyflie2.1 is turned ON. Also, the connection bandwidth is set to `2M`. 
---
+++ crazyflie_backyard_flyer.py
@@ -0,0 +1,192 @@
+import argparse
+import time
+from enum import Enum
+
+import numpy as np
+
+from udacidrone.connection import CrazyflieConnection
+
+from udacidrone import Drone
+from udacidrone.connection import MavlinkConnection, WebSocketConnection  # noqa: F401
+from udacidrone.messaging import MsgID
+
+
+class States(Enum):
+    MANUAL = 0
+    ARMING = 1
+    TAKEOFF = 2
+    WAYPOINT = 3
+    LANDING = 4
+    DISARMING = 5
+
+
+class BackyardFlyer(Drone):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+        self.target_position = np.array([0.0, 0.0, 0.0])
+        self.all_waypoints = []
+        self.in_mission = True
+        self.check_state = {}
+
+        # initial state
+        self.flight_state = States.MANUAL
+
+        # initialize waypoints to navigate
+        self.all_waypoints = self.calculate_box()
+
+        # TODO: Register all your callbacks here
+        self.register_callback(MsgID.LOCAL_POSITION,
+                               self.local_position_callback)
+        self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
+        self.register_callback(MsgID.STATE, self.state_callback)
+
+    def local_position_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
+        """
+        if self.flight_state == States.MANUAL:
+            self.takeoff_transition()
+        if self.flight_state == States.TAKEOFF:
+            altitude = -1 * self.local_position[2]
+            if altitude > 0.95 * self.target_position[2]:
+                # Transition to Waypoint after reaching certain altitude
+                # after takeoff
+                self.waypoint_transition()
+        if self.flight_state == States.WAYPOINT:
+            north_diff = abs(self.local_position[0] - self.target_position[0])
+            east_diff = abs(self.local_position[1] - self.target_position[1])
+            altitude = -1.0 * self.target_position[2]
+            alt_diff = abs(altitude - self.local_position[2])
+            if (north_diff < 0.2 and east_diff < 0.2 and alt_diff < 0.2):
+                self.flight_state = States.TAKEOFF
+
+    def velocity_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
+        """
+        if self.flight_state == States.LANDING:
+            if abs(self.local_position[2] < 0.01):
+                self.manual_transition()
+
+    def state_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
+        """
+        if not self.in_mission:
+            return
+        if self.flight_state == States.ARMING:
+            self.takeoff_transition()
+        if self.flight_state == States.DISARMING:
+            self.manual_transition()
+
+    def calculate_box(self):
+        """TODO: Fill out this method
+
+        1. Return waypoints to fly a box
+            1--------2
+            |        |
+            |        |
+            0--------3
+            4
+        """
+        cp = self.local_position
+        cp[2] = 0
+        local_waypoints = [cp + [0.75, 0.0, 0.5],
+                           cp + [0.75, 0.75, 0.5],
+                           cp + [0.0, 0.75, 0.5],
+                           cp + [0.0, 0.0, 0.5]]
+        return local_waypoints
+
+    def takeoff_transition(self):
+        """TODO: Fill out this method
+
+        1. Set target_position altitude to 3.0m
+        2. Command a takeoff to 3.0m
+        3. Transition to the TAKEOFF state
+        """
+        print("takeoff transition")
+        target_altitude = 0.5
+        self.target_position[2] = target_altitude
+        self.takeoff(target_altitude)
+        # set appropriate state
+        self.flight_state = States.TAKEOFF
+
+    def waypoint_transition(self):
+        """TODO: Fill out this method
+
+        1. Command the next waypoint position
+        2. Transition to WAYPOINT state
+        """
+        print("waypoint transition")
+        if self.all_waypoints:
+            waypoint = self.all_waypoints.pop(0)
+            print(f'waypoint to navigate to: {waypoint}')
+            north, east, altitude = waypoint
+            print(north, east, altitude)
+            self.target_position[0] = north
+            self.target_position[1] = east
+            self.target_position[2] = altitude
+            self.cmd_position(north, east, altitude, 0)
+            self.flight_state = States.WAYPOINT
+        else:
+            self.landing_transition()
+
+    def landing_transition(self):
+        """TODO: Fill out this method
+
+        1. Command the drone to land
+        2. Transition to the LANDING state
+        """
+        print("landing transition")
+        self.land()
+        self.flight_state = States.LANDING
+
+    def manual_transition(self):
+        """This method is provided
+
+        1. Release control of the drone
+        2. Stop the connection (and telemetry log)
+        3. End the mission
+        4. Transition to the MANUAL state
+        """
+        print("manual transition")
+
+        self.release_control()
+        self.stop()
+        self.in_mission = False
+        self.flight_state = States.MANUAL
+
+    def start(self):
+        """This method is provided
+
+        1. Open a log file
+        2. Start the drone connection
+        3. Close the log file
+        """
+        print("Creating log file")
+        self.start_log("Logs", "NavLog.txt")
+        print("starting connection")
+        self.connection.start()
+        print("Closing log file")
+        self.stop_log()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5760, help='Port number')
+    parser.add_argument('--host', type=str, default='127.0.0.1',
+                        help="host address, i.e. '127.0.0.1'")
+    args = parser.parse_args()
+
+    conn = CrazyflieConnection('radio://0/80/2M')
+    #conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
+    drone = BackyardFlyer(conn)
+    time.sleep(2)
+    drone.start()
